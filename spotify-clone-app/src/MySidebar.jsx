34c548876c4ab9c1f813118a95dceea57f51ab40
momoyama <momongamomoyama@gmail.com>
2022-07-19 12:08:46 +0900

    適当な変更

--- spotify-clone-app/src/MySidebar.jsx
+++ spotify-clone-app/src/MySidebar.jsx
@@ -1,40 +1,33 @@
-
-import React, { useEffect } from 'react'
-import "./Sidebar.css"
-import SidebarOption from './SidebarOption'
+import React, { useEffect } from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 import { AiFillHome } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import { BiLibrary } from "react-icons/bi";
 import { getTokenFromResponse } from "./spotify";
-import { useDataLayerValue } from './DataLayer';
-import { Link } from "react-router-dom"
-
-
+import { useDataLayerValue } from "./DataLayer";
+import { Link } from "react-router-dom";
 
 function MySidebar() {
-    return (
-        <div>
-            <div className='sidebar'>
-                <img
-                    className="sidebar__logo"
-                    src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-                    alt=""
-                />
+  return (
+    <div>
+      <div className="sidebar">
+        <img
+          className="sidebar__logo"
+          src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+          alt=""
+        />
 
-                <SidebarOption Icon={AiFillHome} title="Home" />
-                <SidebarOption Icon={BsSearch} title="Search" />
-              
-                <br />
+        <SidebarOption Icon={AiFillHome} title="Home" />
+        <SidebarOption Icon={BsSearch} title="Search" />
 
-                <strong className='sidebar__title'>PLAYLISTS</strong>
-                <hr />
+        <br />
 
-               
-            </div>
-
-        </div>
-
-    )
+        <strong className="sidebar__title">PLAYLISTS</strong>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
-export default MySidebar
+export default MySidebar;
--- spotify-clone-app/src/Sidebar.jsx
+++ spotify-clone-app/src/Sidebar.jsx
@@ -1,44 +1,42 @@
-import React, { useEffect } from 'react'
-import "./Sidebar.css"
-import SidebarOption from './SidebarOption'
+import React, { useEffect } from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 import { AiFillHome } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import { BiLibrary } from "react-icons/bi";
 import { getTokenFromResponse } from "./spotify";
-import { useDataLayerValue } from './DataLayer';
-import { Link } from "react-router-dom"
-
-
+import { useDataLayerValue } from "./DataLayer";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
-    const [{ playlists }, dispatch] = useDataLayerValue();
-    console.log("Playlist", playlists);
-    let p = {
-        items: [1, 2, 3]
-    }
-    return (
-        <div className='sidebar'>
-            <img
-                className="sidebar__logo"
-                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-                alt=""
-            />
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  console.log("Playlist", playlists);
+  let p = {
+    items: [1, 2, 3],
+  };
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
 
-            <SidebarOption Icon={AiFillHome} title="Home" />
-            <SidebarOption Icon={BsSearch} title="Search" />
-            <Link to="MyTrack" >
-                <SidebarOption Icon={BiLibrary} title="MyTrack" />
-            </Link>
-            <br />
+      <SidebarOption Icon={AiFillHome} title="Home" />
+      <SidebarOption Icon={BsSearch} title="Search" />
+      <Link to="MyTrack">
+        <SidebarOption Icon={BiLibrary} title="MyTrack" />
+      </Link>
+      <br />
 
-            <strong className='sidebar__title'>PLAYLISTS</strong>
-            <hr />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
 
-            {playlists?.items?.map((playlist, key) => (
-                <SidebarOption title={playlist.name} />
-            ))}
-        </div>
-    );
+      {playlists?.items?.map((playlist, key) => (
+        <SidebarOption title={playlist.name} />
+      ))}
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
