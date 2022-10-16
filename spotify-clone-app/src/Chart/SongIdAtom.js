b6d73b555bbd52e5df1de6b88b037b266b3b379c
momo <momongamomoyama@gmail.com>
2022-10-16 16:53:38 +0900

    recoil

---
+++ spotify-clone-app/src/Chart/SongIdAtom.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+
+export const songIdAtom = atom({
+key: "songIdAtom",
+default: "",
+});
+
+// 3khEEPRyBeOUabbmOPJzAG
+
+export const tokenAtom = atom({
+    key: "tokenAtom",
+    default: null,
+    });
